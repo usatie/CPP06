7c338d751aa55d782b17d6c91d98da64ca4cccb7
Shun Usami <susami@c1r15s1.42tokyo.jp>
2023-05-06 14:53:03 +0900

    :pencil: Modify ex01 tests

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -39,21 +39,31 @@ void printSubtitle(std::string const& subtitle) {
 
 void test_from_subject() {
   printTitle("Test from sunject pdf");
-  printSubtitle(
-      "The return value of deserialize() compares equal to the original "
-      "pointer");
   unsigned char data[8] = {0, 1, 2, 3, 4, 5, 6, 7};
   Data d;
 
   d.set(data);
-  uintptr_t raw = Serializer::serialize(&d);
-  Data* ptr = Serializer::deserialize(raw);
-  std::cout << "d         : " << d << std::endl;
-  std::cout << "raw       : " << raw << std::endl;
-  std::cout << "&d        : " << &d << std::endl;
-  std::cout << "ptr       : " << ptr << std::endl;
-  std::cout << "*ptr      : " << *ptr << std::endl;
-  std::cout << "&d == ptr : " << (&d == ptr ? "true" : "false") << std::endl;
+
+  printSubtitle("Use serialize() on the address of the Data object and pass its return value to deserialize().");
+  {
+	  if ( &d == Serializer::deserialize(Serializer::serialize(&d)) ) {
+	  	std::cout << GREEN << "the return value of deserialize() is equal to the original pointer." << RESET << std::endl ;
+	  } else {
+	  	std::cout << RED << "the return value of deserialize() is not equal to the original pointer." << RESET << std::endl ;
+	  }
+  }
+
+  printSubtitle("Inspect the pointer values");
+  {
+	  uintptr_t raw = Serializer::serialize(&d);
+	  Data* ptr = Serializer::deserialize(raw);
+	  std::cout << "d         : " << d << std::endl;
+	  std::cout << "raw       : " << raw << std::endl;
+	  std::cout << "&d        : " << &d << std::endl;
+	  std::cout << "ptr       : " << ptr << std::endl;
+	  std::cout << "*ptr      : " << *ptr << std::endl;
+	  std::cout << "&d == ptr : " << (&d == ptr ? "true" : "false") << std::endl;
+  }
 }
 
 int main(void) {
